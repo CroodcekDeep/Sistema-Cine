4752ff4304f53e8a3b9e234392903f0ee5510879
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2022-02-23 17:02:29 -0500

    feat: La estructura de datos de clientes ahora es una DoublyLinkedList

--- src/RegistroVenta.java
+++ src/RegistroVenta.java
@@ -1,10 +1,13 @@
+import AbstractDataStructures.DoublyLinkedList;
+
 import java.io.*;
 import java.util.*;
 
 public class RegistroVenta {
 
-    //TODO: The data structure here should be a DoublyLinkedList instead of an ArrayList
-    private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    private DoublyLinkedList<Cliente> clientes = new DoublyLinkedList<Cliente>();
+
+
     //TODO: The data structure here should be a HashMap instead of an ArrayList
     private ArrayList<Empleado> empleados = new ArrayList<Empleado>();
 
@@ -14,38 +17,42 @@ public class RegistroVenta {
 
     }
 
-    public ArrayList<Cliente> getClientes() {
-        return clientes;
+    /***
+     * Este metodo aniade un nuevo cliente a la Estructura de clientes
+     * @param cliente
+     */
+    public void addCliente(Cliente cliente){
+        this.clientes.addLast(cliente);
     }
 
-    public void setClientes(ArrayList<Cliente> clientes) {
-        this.clientes = clientes;
+    /***
+     * Este metodo retorna la lista de clientes.
+     * @return
+     */
+    public DoublyLinkedList<Cliente> getClientes(){
+        return this.clientes;
     }
 
-    public ArrayList<Empleado> getEmpleados() {
-        return empleados;
-    }
 
-    public void setEmpleados(ArrayList<Empleado> empleados) {
-        this.empleados = empleados;
-    }
+
+
 
     public static void main(String arg[]){
         RegistroVenta test = new RegistroVenta();
-        
+
         Cliente c1 = new Cliente("Carlos", "Mendoza", "1316812104", "Av 5", "0987868186");
         Cliente c2 = new Cliente("Carla", "Mendoza", "1316812104", "Av 5", "0987868186");
         Cliente c3 = new Cliente("Luca", "Mendoza", "1316812104", "Av 5", "0987868186");
 
-        ArrayList<Cliente> listaClientes = new ArrayList<>();
-        listaClientes.add(c1);
-        listaClientes.add(c2);
-        listaClientes.add(c3);
-        test.setClientes(listaClientes);
+        test.addCliente(c1);
+        test.addCliente(c2);
+        test.addCliente(c3);
 
-        // Logica de encapsulacion, al final esta dentro de la clase RegistroVenta
-        for(int i=0; i<test.getClientes().size(); i++){
-            System.out.println(test.getClientes().get(i).getNombres());
-        }
+        test.getClientes().show();
+        System.out.println();
+        System.out.println(test.getClientes().get(2).getNombres());
+
+
+
     }
 }
