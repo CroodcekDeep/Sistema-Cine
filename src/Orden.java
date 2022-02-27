71052b03b8098552c0b8d7ebe04dce5e6ca58869
Gandhy <gandhy.garcia@epn.edu.ec>
2022-02-26 21:22:33 -0500

    feat: Add Factura and modify Orden and Producto classes
    Se añadio la clase Factura con sus atributos y métodos.
    Además, se modificó la clase Orden y se añadieron los métodos anadirProducto() y calcularTotal().
    Por último, se modificó el método toString() en la clase Producto
    
    Gandhy García

--- src/Orden.java
+++ src/Orden.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Orden {
     private int ordenID;
     private Cliente cliente;
-    //TODO: Anaidir producto
+    private Empleado empleado;
+    private ArrayList<Producto> productos;
 
-    public Orden(int ordenID, Cliente cliente){
+    public Orden(int ordenID, Cliente cliente, Empleado empleado, ArrayList<Producto> productos) {
         this.ordenID = ordenID;
         this.cliente = cliente;
+        this.empleado = empleado;
+        this.productos = productos;
     }
 
     public int getOrdenID() {
@@ -22,5 +28,44 @@ public class Orden {
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public void anadirProducto(Producto producto) {
+        this.productos.add(producto);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Producto p : productos) {
+            total += p.getPrecio();
+        }
+
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "\u001B[33mOrden:" +
+                "\n\t\tIdOrden: " + ordenID +
+                "\n\t\tCliente: " + cliente +
+                "\n\t\tEmpleado: " + empleado +
+                "\n\t\tProductos: " + productos +
+                "\n\u001B[0m";
     }
 }
