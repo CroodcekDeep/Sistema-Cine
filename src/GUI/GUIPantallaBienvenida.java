d3c0ecc43076a14e148ccc348a0f3d41bdc9e107
Pablo <pablo.velez01@epn.edu.ec>
2022-02-27 14:08:17 -0500

    feat: Funcionalidad en pantalla de Bienvenida
    Pablo Vélez

--- src/GUI/GUIPantallaBienvenida.java
+++ src/GUI/GUIPantallaBienvenida.java
@@ -2,6 +2,14 @@ package GUI;
 
 import javax.swing.*;
 
-public class GUIPantallaBienvenida {
+public class GUIPantallaBienvenida extends JFrame{
     private JPanel panel1;
+    private JButton btnCatalogo;
+    private JButton btnRegistro;
+
+   public GUIPantallaBienvenida(){
+       super("Patalla de Bienvenida");
+
+       setContentPane(panel1);
+   }
 }
